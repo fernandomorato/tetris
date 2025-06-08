40f38fc976c8d440be281e0f082c209c5e5a6fdc
Fernando Morato <fernando.morato@incognia.com>
2025-06-08 19:53:17 -0300

    wip

--- tetris/piece.go
+++ tetris/piece.go
@@ -1,89 +1,104 @@
 package tetris
 
+import (
+	"fmt"
+	"math"
+)
+
 type Piece [4]Position
 
-var Tetrominoes = [7]Piece{
-	{
-		//  . . . .
-		//  . . . .
-		// [][][][]
-		//  . . . .
-		{2, 0},
-		{2, 1},
-		{2, 2},
-		{2, 3},
-	},
-	{
-		//  . .[]
-		// [][][]
-		//  . . .
-		{0, 2},
-		{1, 0},
-		{1, 1},
-		{1, 2},
-	},
-	{
-		//  .[] .
-		// [][][]
-		//  . . .
-		{0, 1},
-		{1, 0},
-		{1, 1},
-		{1, 2},
-	},
-	{
-		// [] . .
-		// [][][]
-		//  . . .
-		{0, 0},
-		{1, 0},
-		{1, 1},
-		{1, 2},
-	},
-	{
-		// [][] .
-		//  .[][]
-		//  . . .
-		{0, 0},
-		{0, 1},
-		{1, 1},
-		{1, 2},
-	},
-	{ // [][]
-		// [][]
-		{0, 0},
-		{0, 1},
-		{1, 0},
-		{1, 1},
-	},
-	{
-		//  .[][]
-		// [][] .
-		//  . . .
-		{0, 1},
-		{0, 2},
-		{1, 0},
-		{1, 1},
-	},
-}
+var (
+	//	. . . .
+	//	. . . .
+	// [][][][]
+	//	. . . .
+	I = Piece{{0, 0}, {0, 1}, {0, 2}, {0, 3}}
+	// [][][]
+	// [] . .
+	//  . . .
+	L = Piece{{0, 0}, {0, 1}, {0, 2}, {1, 0}}
+	// [][][]
+	//  .[] .
+	//  . . .
+	T = Piece{{0, 0}, {0, 1}, {0, 2}, {1, 1}}
+	// [][][]
+	//  . .[]
+	//  . . .
+	J = Piece{{0, 0}, {0, 1}, {0, 2}, {1, 2}}
+	// [][] .
+	//  .[][]
+	//  . . .
+	Z = Piece{{0, 0}, {0, 1}, {1, 1}, {1, 2}}
+	// [][]
+	// [][]
+	O = Piece{{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+	//  .[][]
+	// [][] .
+	//  . . .
+	S = Piece{{0, 1}, {0, 2}, {1, 0}, {1, 1}}
+)
 
-func (p *Piece) rotation() Piece {
-	if *p == Tetrominoes[5] {
-		// square
-		return *p
-	}
-	// Rotation: (x, y) -> (y, -x), centered on (cx, cy)
-	newPiece := Piece{}
-	cx := 1.0
-	cy := 1.0
-	if *p == Tetrominoes[0] {
-		// line
-		cx = 1.5
-		cy = 1.5
+var Tetrominoes = [7]Piece{I, L, T, J, Z, O, S}
+
+func (p *Piece) rotate() {
+	minX := math.MaxInt
+	minY := math.MaxInt
+	for i := range p {
+		minX = min(minX, p[i].x)
+		minY = min(minY, p[i].y)
 	}
 	for i := range p {
-		newPiece[i].x = int(float64(p[i].y) - cy + cx)
-		newPiece[i].y = int(-(float64(p[i].x) - cx) + cy)
+		p[i].x -= minX
+		p[i].y -= minY
 	}
-	return newPiece
+	var cx, cy float64
+	switch *p {
+	case T, S, Z, L, J:
+		cx, cy = 0.5, 1.5
+	case I:
+		cx, cy = -0.5, 1.5
+	case O:
+		cx, cy = 0.5, 0.5
+	}
+	fmt.Println(cx, cy)
+
+	// Rotation: (x, y) -> (y, -x), centered on (cx, cy)
+
+	for i := range p {
+		fmt.Printf("%d with %v\n", i, p[i])
+		nx := float64(p[i].x) - cx
+		ny := float64(p[i].y) - cy
+		fmt.Println(nx, ny)
+		nx, ny = ny, -nx
+		fmt.Println(nx, ny)
+		nx += cx
+		ny += cy
+		fmt.Println(nx, ny)
+		p[i] = Position{
+			x: int(nx),
+			y: int(ny),
+		}
+	}
+	for i := range p {
+		p[i].x += minX
+		p[i].y += minY
+	}
+}
+
+func (p *Piece) drop() {
+	for i := range p {
+		p[i].x++
+	}
+}
+
+func (p *Piece) left() {
+	for i := range p {
+		p[i].y--
+	}
+}
+
+func (p *Piece) right() {
+	for i := range p {
+		p[i].y++
+	}
 }
